4c08c1772c9512604bbab97c9667c97eb441be8b
Zeohye <Nilshenning@live.dk>
2014-12-13 15:26:54 +0100

    All done, now with all experiments with fold,score and time.
    GL if any tries to refactor this mess

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,134 @@
 public class Main {
 
     public static void main(String[] args) {
-        String seq = "hhhpphphphpphphphpph";
+        System.out.println("Problem 1:");
+        String seq = "hphpphhphpphphhpphph";
+        long sTime = System.currentTimeMillis();
         String result = ApproxHPFold.fold(seq);
-/*
-        seq = "hphhhhhh";
-        result = ApproxHPFold.fold(seq);
-        System.out.println(result);*/
-
+        long eTime = System.currentTimeMillis();
+        String time = "Time to calculate fold: " + (eTime-sTime) +"ms";
+        sTime = System.currentTimeMillis();
         ApproxHPFold.printFolding(seq,result);
+        eTime = System.currentTimeMillis();
+        System.out.println(time);
+        System.out.println("Time to print fold, and calculate score: " + (eTime-sTime) +"ms");
+
+        System.out.println();
+        System.out.println("Problem 2:");
+        seq = "hhhpphphphpphphphpph";
+        sTime = System.currentTimeMillis();
+        result = ApproxHPFold.fold(seq);
+        eTime = System.currentTimeMillis();
+        time = "Time to calculate fold: " + (eTime-sTime) +"ms";
+        sTime = System.currentTimeMillis();
+        ApproxHPFold.printFolding(seq,result);
+        eTime = System.currentTimeMillis();
+        System.out.println(time);
+        System.out.println("Time to print fold, and calculate score: " + (eTime - sTime) + "ms");
+
+        System.out.println();
+        System.out.println("Problem 3:");
+        seq = "ppphhpphhppppphhhhhhhpphhpppphhpphpp";
+        sTime = System.currentTimeMillis();
+        result = ApproxHPFold.fold(seq);
+        eTime = System.currentTimeMillis();
+        time = "Time to calculate fold: " + (eTime-sTime) +"ms";
+        sTime = System.currentTimeMillis();
+        ApproxHPFold.printFolding(seq,result);
+        eTime = System.currentTimeMillis();
+        System.out.println(time);
+        System.out.println("Time to print fold, and calculate score: " + (eTime - sTime) + "ms");
+
+        System.out.println();
+        System.out.println("Problem 4:");
+        seq = "hhphphphphhhhphppphppphpppphppphppphphhhhphphphphh";
+        sTime = System.currentTimeMillis();
+        result = ApproxHPFold.fold(seq);
+        eTime = System.currentTimeMillis();
+        time = "Time to calculate fold: " + (eTime-sTime) +"ms";
+        sTime = System.currentTimeMillis();
+        ApproxHPFold.printFolding(seq,result);
+        eTime = System.currentTimeMillis();
+        System.out.println(time);
+        System.out.println("Time to print fold, and calculate score: " + (eTime - sTime) + "ms");
+
+        System.out.println();
+        System.out.println("Problem 5:");
+        seq = "hhhhhhhhhhhhphphpphhpphhpphpphhpphhpphpphhpphhpphphphhhhhhhhhhhh";
+        sTime = System.currentTimeMillis();
+        result = ApproxHPFold.fold(seq);
+        eTime = System.currentTimeMillis();
+        time = "Time to calculate fold: " + (eTime-sTime) +"ms";
+        sTime = System.currentTimeMillis();
+        ApproxHPFold.printFolding(seq,result);
+        eTime = System.currentTimeMillis();
+        System.out.println(time);
+        System.out.println("Time to print fold, and calculate score: " + (eTime - sTime) + "ms");
+
+        System.out.println();
+        System.out.println("Sequence 1:");
+        seq = "pphpphhpppphhpppphhpppphh";
+        sTime = System.currentTimeMillis();
+        result = ApproxHPFold.fold(seq);
+        eTime = System.currentTimeMillis();
+        time = "Time to calculate fold: " + (eTime-sTime) +"ms";
+        sTime = System.currentTimeMillis();
+        ApproxHPFold.printFolding(seq,result);
+        eTime = System.currentTimeMillis();
+        System.out.println(time);
+        System.out.println("Time to print fold, and calculate score: " + (eTime - sTime) + "ms");
+
+        System.out.println();
+        System.out.println("Sequence 2:");
+        seq = "pphhhphhhhhhhhppphhhhhhhhhhphppphhhhhhhhhhhhpppphhhhhhphhphp";
+        sTime = System.currentTimeMillis();
+        result = ApproxHPFold.fold(seq);
+        eTime = System.currentTimeMillis();
+        time = "Time to calculate fold: " + (eTime-sTime) +"ms";
+        sTime = System.currentTimeMillis();
+        ApproxHPFold.printFolding(seq,result);
+        eTime = System.currentTimeMillis();
+        System.out.println(time);
+        System.out.println("Time to print fold, and calculate score: " + (eTime - sTime) + "ms");
+
+        System.out.println();
+        System.out.println("Sequence 3:");
+        seq = "hhhhpppphhhhhhhhhhhhpppppphhhhhhhhhhhhppphhhhhhhhhhhhppphhhhhhhhhhhhppphpphhpphhpphph";
+        sTime = System.currentTimeMillis();
+        result = ApproxHPFold.fold(seq);
+        eTime = System.currentTimeMillis();
+        time = "Time to calculate fold: " + (eTime-sTime) +"ms";
+        sTime = System.currentTimeMillis();
+        ApproxHPFold.printFolding(seq,result);
+        eTime = System.currentTimeMillis();
+        System.out.println(time);
+        System.out.println("Time to print fold, and calculate score: " + (eTime - sTime) + "ms");
+
+        System.out.println();
+        System.out.println("Sequence 4:");
+        seq = "pppppphphhppppphhhphhhhhphhpppphhpphhphhhhhphhhhhhhhhhphhphhhhhhhppppppppppphhhhhhhpphphhhpppppphphh";
+        sTime = System.currentTimeMillis();
+        result = ApproxHPFold.fold(seq);
+        eTime = System.currentTimeMillis();
+        time = "Time to calculate fold: " + (eTime-sTime) +"ms";
+        sTime = System.currentTimeMillis();
+        ApproxHPFold.printFolding(seq,result);
+        eTime = System.currentTimeMillis();
+        System.out.println(time);
+        System.out.println("Time to print fold, and calculate score: " + (eTime - sTime) + "ms");
+
+        System.out.println();
+        System.out.println("Sequence 5:");
+        seq = "ppphhpphhhhpphhhphhphhphhhhpppppppphhhhhhpphhhhhhppppppppphphhphhhhhhhhhhhpphhhphhphpphphhhpppppphhh";
+        sTime = System.currentTimeMillis();
+        result = ApproxHPFold.fold(seq);
+        eTime = System.currentTimeMillis();
+        time = "Time to calculate fold: " + (eTime-sTime) +"ms";
+        sTime = System.currentTimeMillis();
+        ApproxHPFold.printFolding(seq,result);
+        eTime = System.currentTimeMillis();
+        System.out.println(time);
+        System.out.println("Time to print fold, and calculate score: " + (eTime - sTime) + "ms");
     }
 
 }
